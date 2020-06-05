37c98a972b29eed33922dd9474ccae68dbcc8c4d
Daniele Hamm <daniele.hamm@epfl.ch>
2020-06-05 18:47:17 +0200

    LBFGS minibatch

--- helpers_opt_parameters.py
+++ helpers_opt_parameters.py
@@ -4,6 +4,7 @@ from torch import nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torch import  optim
+from torch.utils import data
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -90,6 +91,26 @@ def optimize_CNN(optimizer, epochs, trainloader, valloader, model, criterion, me
         running_loss = 0
         for images, labels in trainloader:
             
+            def closure():
+                # Training pass
+                optimizer.zero_grad()
+                output = model(images)
+                loss = criterion(output, labels)
+                #This is where the model learns by backpropagating, not needed for linesearch
+                if loss.requires_grad:
+                    loss.backward()
+                return loss
+
+            def closure_hf():
+                # Training pass
+
+                output = model(images)
+                loss = criterion(output, labels)
+                #This is where the model learns by backpropagating, not needed for linesearch
+                if loss.requires_grad:
+                    loss.backward(create_graph=True)
+                return loss , output
+            
             def closure_sd():
                 optimizer.zero_grad()
                 output = model(images)
@@ -97,16 +118,29 @@ def optimize_CNN(optimizer, epochs, trainloader, valloader, model, criterion, me
                 loss.backward()
                 return loss
 
-            if method == "SdLBFGS":
-               
+            #And optimizes its weights here
+            if method== "LBFGS" :
+                optimizer.step(closure)
+
+            elif method == "HF" :
+                optimizer.zero_grad()
+                optimizer.step(closure_hf, M_inv=None)
+
+            elif method == "SdLBFGS" :
+                #optimizer.zero_grad()
+                #output = model(images)
+                #loss = criterion(output, labels)
+                #loss.backward()
                 optimizer.step(closure_sd)
                 
-            else:
-                optimizer.zero_grad()
-                output = model(images)
-                loss = criterion(output, labels)
-                #This is where the model learns by backpropagating
-                loss.backward()#And optimizes its weights here
+            elif method == "CurveBall":
+                # create closures to compute the forward pass, and the loss
+                model_fn = lambda: model(images)
+                loss_fn = lambda pred: criterion(pred, labels)
+                (loss, predictions) = optimizer.step(model_fn, loss_fn)
+
+            else :
+                closure()
                 optimizer.step()
            
             
@@ -249,6 +283,7 @@ def optimize(optimizer, epochs, trainloader, valloader, model, criterion , metho
 
 
     print("\nTraining Time (in minutes) =",(time()-time0)/60)
+    print("\n")
     training_time = (time()-time0)/60
 
     return train_losses, test_losses, train_accuracy, test_accuracy,training_time
@@ -408,3 +443,51 @@ def learning_rate_optimization_SGD_CNN(input_size, output_size, trainloader, val
           #  best_learning_rate = lr
     return  training_loss, test_loss,training_accuracy, test_accuracy,times
 
+
+def hyperparameters_tuning_LBFGS_minibatch(trainset, valset, batchsize_grid, history_size_grid, epochs, model_NN):
+    
+    
+    training_loss =[]
+    test_loss = []
+    training_accuracy = []
+    test_accuracy = []
+    times = []
+
+    
+    for bs in batchsize_grid:
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True)
+        valloader = torch.utils.data.DataLoader(valset, batch_size=bs, shuffle=True)    
+        dataiter = iter(trainloader)
+        images,_=dataiter.next()
+        image_size=images[0].shape[1]
+        input_size = int(image_size**2)
+        output_size = 10
+        for hs in history_size_grid:
+            print("Minibatch size: ", bs)
+            print("History size: ",hs)
+            
+            if model_NN=="FCNN":
+                sizes = [input_size,128,64,output_size]
+                model = fully_connected_NN(sizes)
+                criterion = nn.NLLLoss()
+                optimizer=optim.LBFGS(model.parameters(),max_iter=10,history_size=hs, line_search_fn='strong_wolfe')
+                
+            elif model_NN=="CNN":
+                model=ConvNet(image_size)
+                criterion = nn.CrossEntropyLoss()
+                optimizer=optim.LBFGS(model.parameters(),max_iter=10,history_size=hs)
+               
+           
+            if model_NN=="FCNN":
+                train_losses, test_losses, train_accuracies, test_accuracies,train_time=optimize(optimizer, epochs, trainloader, valloader, model,criterion,method = "LBFGS")
+            elif model_NN=="CNN":
+                train_losses, test_losses, train_accuracies, test_accuracies,train_time=optimize_CNN(optimizer, epochs, trainloader, valloader, model,criterion,method = "LBFGS")
+                
+        
+            times.append(train_time)
+            training_loss.append(train_losses)
+            test_loss.append(test_losses)
+            training_accuracy.append(train_accuracies)
+            test_accuracy.append( test_accuracies )
+       
+    return  training_loss, test_loss,training_accuracy, test_accuracy ,times
